c89ad04c9474432220c38201f4ce0b200d2b904f
Yuan Yao <yaoyuan0329os@gmail.com>
2024-03-20 22:08:21 +0800

    AKVM: UAPI: Add comments to akvm uapi
    Signed-off-by: Yuan Yao <yaoyuan0329os@gmail.com>

--- include/uapi/linux/akvm.h
+++ include/uapi/linux/akvm.h
@@ -16,17 +16,24 @@
 
 #define AKVMIO 0xCC
 
-#define AKVM_RUN		_IO(AKVMIO,   0x00)
+/* per vcpu level: */
+#define AKVM_RUN		_IO(AKVMIO, 0x00)
 
+/* akvm dev level: */
 struct akvm_vmx_info {
 	__u64 vmx_basic_msr;
 	__u64 vmx_misc_msr;
 	__u64 vmx_ept_vpid_msr;
 };
-#define AKVM_GET_VMX_INFO	_IOR(AKVMIO,  0x01, struct akvm_vmx_info)
-#define AKVM_CREATE_VM   _IO(AKVMIO,   0x02)
-#define AKVM_CREATE_VCPU _IO(AKVMIO,   0x03)
+#define AKVM_GET_VMX_INFO	_IOR(AKVMIO, 0x01, struct akvm_vmx_info)
 
+/* akvm dev level: */
+#define AKVM_CREATE_VM   _IO(AKVMIO, 0x02)
+
+/* per vm level: */
+#define AKVM_CREATE_VCPU _IO(AKVMIO, 0x03)
+
+/* per vm level: */
 struct akvm_memory_slot {
 	__u64 hva;
 	__u64 gpa;
@@ -35,10 +42,11 @@ struct akvm_memory_slot {
 };
 #define AKVM_MEMORY_SLOT_ADD _IOR(AKVMIO, 0x4, struct akvm_memory_slot)
 #define AKVM_MEMORY_SLOT_REMOVE _IOR(AKVMIO, 0x5, struct akvm_memory_slot)
-#define AKVM_VCPU_SET_RIP _IOR(AKVMIO, 0x6, long)
-
-/* hva/gpa/size need aligning on this value */
+/* hva/gpa/size need align on this value */
 #define AKVM_MEMORY_SLOT_ALIGN 4096
+
+/* per vcpu */
+#define AKVM_VCPU_SET_RIP	_IOR(AKVMIO, 0x6, long)
 
 #define VM_SERVICE_SUCCESS 0LL
 
